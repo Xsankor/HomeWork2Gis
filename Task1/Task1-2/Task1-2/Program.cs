87f6e275e62a85b36d6ea435456e846e12a6ebb1
julTitova <titova_u_m@mail.ru>
2015-06-04 03:56:32 +0600

    Task1 - сумма двух чисел, введенных с консоли Task1-2  - разбиение 64х-битного числа на отдельные id

---
+++ Task1/Task1-2/Task1-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong Number = 1267165676175383;
+            byte TypeId = Convert.ToByte(Number >> 62);
+            ulong CityId = Number << 2;
+            CityId = CityId >> 49;
+            ulong TableId = Number << 17;
+            TableId = TableId >> 49;
+            ulong ObjectId = Number << 32;
+            ObjectId = ObjectId >> 32;
+            Console.WriteLine("Id типа: " + TypeId);
+            Console.WriteLine("Id города: " + CityId);
+            Console.WriteLine("Id таблицы: " + TableId);
+            Console.WriteLine("Id записи (объекта): " + ObjectId);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task1
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            // Задание: Ввести с консоли числа вывести их сумму
+            Console.WriteLine("Введите первое число");
+            string a = Console.ReadLine();
+            Console.WriteLine("Введите второе число");           
+            string b = (Console.ReadLine());
+            try
+            {
+                double FirstNumber = Convert.ToDouble(a);
+                double SecondNumber = Convert.ToDouble(b);
+                double Result;
+                Result = FirstNumber + SecondNumber;
+                Console.WriteLine("Сумма: " + Result);
+            }
+            catch { Console.WriteLine("Ошибка! Можно вводить вводить только числа"); }
+            Console.ReadKey();
+        }
+    }
+}
