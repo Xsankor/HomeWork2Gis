e9bae20c53a6187c0975dd0763db5412b2020a45
julTitova <titova_u_m@mail.ru>
2015-06-09 02:29:41 +0600

    Нахождение наименьшего элемента каждой строки матрицы

---
+++ Task2/MinMatrix/MinMatrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 7;
+            int[,] Matrix = new int[N, N];
+            Random rand = new Random();
+            string Result = "Минимальные значения в каждой страке: \n";
+            string StrMatrix = "Матрица: \n";
+            for (int i = 0; i < N; i++)
+            {
+                int[] LocalArray = new int[N];
+                for (int j = 0; j < N; j++)
+                {
+                    Matrix[i, j] = rand.Next(100);
+                    LocalArray[j] = Matrix[i, j];
+                    StrMatrix = StrMatrix + Matrix[i, j] + '\t';
+                    if (j == 6)
+                    {
+                        Array.Sort(LocalArray);
+                        Result = Result + LocalArray[0] + '\n';
+                        StrMatrix = StrMatrix + '\n';
+                    }
+                }
+            }
+            Console.WriteLine(StrMatrix);
+            Console.WriteLine(Result);
+            Console.ReadKey();
+    
+        }
+    }
+}
