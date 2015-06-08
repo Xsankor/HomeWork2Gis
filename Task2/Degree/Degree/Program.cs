de58d25788712602979fffc290ea0e14c8d42586
julTitova <titova_u_m@mail.ru>
2015-06-09 02:16:53 +0600

    Возведение числа в степень

---
+++ Task2/Degree/Degree/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Degree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Number = 0, Degree = 0, Result = 0;
+            Console.WriteLine("Введите X - число, которое хотите возвести в степень");
+            try 
+            {
+                Number = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите N - степень, в которую хотите возвести число");
+                Degree = Convert.ToDouble(Console.ReadLine());
+                Result = Math.Pow(Number, Degree);
+                if (Convert.ToString(Result) == "NaN")
+                {
+                    Console.WriteLine("Данную операцию невозможно выполнить. Результат - не определен!");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Х в степени N = " + Result);
+                    Console.ReadKey();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введенное значение не соответствует формату числа!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
