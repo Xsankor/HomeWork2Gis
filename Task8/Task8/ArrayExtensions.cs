c51fa146193e051b1099d30746bf5bad97f016fb
julTitova <titova_u_m@mail.ru>
2015-07-29 10:12:57 +0600

    Отдельный проект по лекции 8, на extension

---
+++ Task8/Task8/ArrayExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task8
+{
+    public static class ArrayExtensions
+    {
+        public static string Print(int[] array, char cyrsor)
+        {
+            var result = string.Empty;
+            foreach (var element in array)
+            {
+                result = result + element.ToString() + cyrsor;
+            }
+            return result;
+        }
+    }
+}
---
+++ Task8/Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(ArrayExtensions.Print(mas, '\n'));
+            Console.ReadKey();
+        }
+    }
+}
